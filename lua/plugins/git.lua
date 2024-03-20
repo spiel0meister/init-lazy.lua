c691c6ce7c8043b4a0e44643950007927d8ce763
spiel0meister <soviczan7@gmail.com>
2024-03-20 17:49:51 +0100

    added keymaps for gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -4,6 +4,10 @@ return {
         "lewis6991/gitsigns.nvim"
     },
     config = function()
-        require("gitsigns").setup()
+        local gitsigns = require("gitsigns")
+        gitsigns.setup()
+
+        vim.keymap.set("n", "<leader>gh", "<cmd>Gitsigns preview_hunk<CR>", { desc = "Previe hunk" })
+        vim.keymap.set("n", "<leader>gtb", "<cmd>Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle current line blame" })
     end
 }
