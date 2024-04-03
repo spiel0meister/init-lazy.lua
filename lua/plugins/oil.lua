fef4491938b590879b75dcd1ab281d6711bf4466
spiel0meister <spiel0meister@gmail.com>
2024-04-03 23:14:25 +0200

    modified oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,7 +4,16 @@ return {
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-      require("oil").setup({})
+      require("oil").setup({
+          columns = {
+              "icon",
+              "size",
+              "mtime",
+          },
+          view_options = {
+              show_hidden = true
+          }
+      })
 
       vim.keymap.set("n", "<leader>pv", require("oil").open, { desc = "Open parent directory" })
   end
