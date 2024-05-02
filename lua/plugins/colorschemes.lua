8729d9e75354b55e04260daf38c7aedeb8040b88
Žan Sovič <soviczan7@gmail.com>
2024-05-02 23:41:24 +0200

    better colorscheme managment

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "catppuccin/nvim",
+    },
+    {
+        "projekt0n/github-nvim-theme",
+        config = function()
+            vim.cmd("colorscheme github_dark_default")
+        end
+    },
+    {
+        "folke/tokyonight.nvim",
+    },
+    {
+        "dracula/vim",
+    },
+}
