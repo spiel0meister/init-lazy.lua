f325ef284f4c641946eaeeaddf2f5d55c2e8ce75
Žan Sovič <soviczan7@gmail.com>
2024-05-07 15:21:02 +0200

    added treesitter-nasm

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
     config = function()
         require('nvim-treesitter.configs').setup {
             -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "rust", "html", "css" },
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "rust", "html", "css", "nasm" },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
