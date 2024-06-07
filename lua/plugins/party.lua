d86debcbce2d49f0e3e63cdd53fa601565a5685b
Žan Sovič <soviczan7@gmail.com>
2024-06-07 23:24:37 +0200

    fixes

--- lua/plugins/party.lua
+++ lua/plugins/party.lua
@@ -1,15 +1,35 @@
 return {
-    "ggandor/leap.nvim",
-    dependencies = {
+    {
+        "ggandor/leap.nvim",
+    },
+    {
         "folke/todo-comments.nvim",
+        config = function ()
+            require("todo-comments").setup()
+        end
+    },
+    {
         'goolord/alpha-nvim',
+        config = function ()
+            require("alpha").setup(require("alpha.themes.startify").config)
+        end
+    },
+    {
         'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
+        config = function ()
+            require("ibl").setup()
+        end
+    },
+    {
         "mg979/vim-visual-multi",
+    },
+    {
         "tpope/vim-commentary"
     },
-    config = function()
-        require("alpha").setup(require("alpha.themes.startify").config)
-        require("ibl").setup()
-        require("todo-comments").setup()
-    end
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    }
 }
