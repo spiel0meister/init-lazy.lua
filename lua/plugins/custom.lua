a6162a2997c8a10ff17156e8472a591d448d655b
spiel0meister <soviczan7@gmail.com>
2025-03-15 19:43:28 +0100

    added desc

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -9,11 +9,11 @@ return {
             local direr = require("direr")
             direr.setup()
 
-            vim.keymap.set("n", "<leader>df", direr.find_files)
-            vim.keymap.set("n", "<leader>ds", direr.live_grep)
-            vim.keymap.set("n", "<leader>dd", direr.add_cur_dir)
-            vim.keymap.set("n", "<leader>dr", direr.remove_cur_dir)
-            vim.keymap.set("n", "<leader>dl", direr.toggle_show_dirs)
+            vim.keymap.set("n", "<leader>df", direr.find_files, { desc = "Fuzzy search saved files" })
+            vim.keymap.set("n", "<leader>ds", direr.live_grep, { desc = "Live grep saved files" })
+            vim.keymap.set("n", "<leader>dd", direr.add_cur_dir, { desc = "Add current directory" })
+            vim.keymap.set("n", "<leader>dr", direr.remove_cur_dir, { desc = "Remove current directory" })
+            vim.keymap.set("n", "<leader>dl", direr.toggle_show_dirs, { desc = "Showing directories" })
         end
     }
 }
