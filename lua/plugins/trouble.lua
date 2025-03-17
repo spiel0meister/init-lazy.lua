2a58142322526bb08ce5959fe7a56ef22b082887
spiel0meister <soviczan7@gmail.com>
2025-03-17 02:32:19 +0100

    added trouble.nvim

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,9 @@
+return {
+    "folke/trouble.nvim",
+    config = function()
+        local trouble = require("trouble")
+        trouble.setup()
+
+        vim.keymap.set("n", "<leader>tt", function() trouble.toggle("diagnostics") end)
+    end
+}
