7d9df0b326cedda22da5fb75c85959a631c892d1
spiel0meister <soviczan7@gmail.com>
2025-03-27 17:11:39 +0100

    new keymap for direr

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -11,9 +11,19 @@ return {
 
             vim.keymap.set("n", "<leader>df", direr.find_files, { desc = "Fuzzy search saved files" })
             vim.keymap.set("n", "<leader>ds", direr.live_grep, { desc = "Live grep saved files" })
+
             vim.keymap.set("n", "<leader>dd", direr.add_cur_dir, { desc = "Add current directory" })
             vim.keymap.set("n", "<leader>dr", direr.remove_cur_dir, { desc = "Remove current directory" })
-            vim.keymap.set("n", "<leader>dl", direr.toggle_show_dirs, { desc = "Showing directories" })
+            vim.keymap.set("n", "<leader>da", function()
+                local dir = vim.fn.input("Enter path of directory: ")
+                if vim.fn.isdirectory(dir) == 1 then
+                    direr.add_dir(dir)
+                else
+                    print("Directory does not exist or is not a directory")
+                end
+            end)
+
+            vim.keymap.set("n", "<leader>dl", direr.toggle_show_dirs, { desc = "Toggle showing directories" })
         end
     }
 }
