79d8bf5c513a576e4170a361414d1682fecaf0a9
spiel0meister <soviczan7@gmail.com>
2025-03-29 23:07:51 +0100

    added spinner for codecompanion; remove codeium stuff

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,11 +1,17 @@
-local function codeium_status()
-    local status = vim.api.nvim_call_function("codeium#GetStatusString", {})
-    if #status == 3 then
-        return "Codeium: " .. status
-    else
-        return "Codeium:" .. status
-    end
-end
+local spinner_table = {
+    "⠋",
+    "⠙",
+    "⠹",
+    "⠸",
+    "⠼",
+    "⠴",
+    "⠦",
+    "⠧",
+    "⠇",
+    "⠏",
+}
+
+local spinner_count = 10
 
 return {
     'nvim-lualine/lualine.nvim',     -- Fancier statusline
@@ -13,7 +19,37 @@ return {
     config = function()
         local lualine = require("lualine")
 
-        lualine.setup {
+        local code_companion_status = {
+            processing = false,
+            spinner_index = 1
+        }
+
+        local group = vim.api.nvim_create_augroup("CodeCompanionStatus", {
+            clear = false,
+        })
+
+        vim.api.nvim_create_autocmd({ "User" }, {
+            pattern = "CodeCompanionRequest*",
+            group = group,
+            callback = function(request)
+                if request.match == "CodeCompanionRequestStarted" then
+                    code_companion_status.processing = true
+                elseif request.match == "CodeCompanionRequestFinished" then
+                    code_companion_status.processing = false
+                end
+            end,
+        })
+
+        local function code_companion_status_update()
+            if code_companion_status.processing then
+                code_companion_status.spinner_index = (code_companion_status.spinner_index % spinner_count) + 1
+                return spinner_table[code_companion_status.spinner_index]
+            else
+                return nil
+            end
+        end
+
+        lualine.setup({
             options = {
                 icons_enabled = true,
                 theme = 'auto',
@@ -36,7 +72,7 @@ return {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch', 'diff', 'diagnostics' },
                 lualine_c = { 'filename' },
-                lualine_x = { 'encoding', 'fileformat', 'filetype', codeium_status },
+                lualine_x = { 'encoding', 'fileformat', 'filetype', code_companion_status_update },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
             },
@@ -52,6 +88,6 @@ return {
             winbar = {},
             inactive_winbar = {},
             extensions = {}
-        }
+        })
     end
 }
