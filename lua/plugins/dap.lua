9d1dd71097fcb576c26909b126e43d1c61f16b8e
spiel0meister <soviczan7@gmail.com>
2025-04-05 15:37:29 +0200

    added dap

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,11 @@
+return {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio"
+    },
+    config = function()
+        local dap = require("dap")
+        require("dapui").setup()
+    end
+}
