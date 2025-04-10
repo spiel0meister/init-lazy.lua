8cd819601e917a6b6fb49750e6c83e1459e0951e
spiel0meister <soviczan7@gmail.com>
2025-04-10 20:36:07 +0200

    removed cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,45 +1,3 @@
-local function setup_custom_snippets(ls)
-    local extras = require("luasnip.extras")
-    local s = ls.snippet
-    local t = ls.text_node
-    local i = ls.insert_node
-    local rep = extras.rep
-
-    local fmt = require("luasnip.extras.fmt").fmt
-
-    ls.add_snippets("c", {
-        s("epf", fmt("fprintf(stderr, \"{}\", {});{}", {
-            i(1),
-            i(2),
-            i(0)
-        })),
-
-        s("struct", fmt([[
-        typedef struct {} {};
-        struct {} {{
-            {}
-        }};
-        ]],
-        {
-            rep(1),
-            rep(1),
-            i(1),
-            i(0),
-        })),
-
-        s("guard", fmt([[
-        #ifndef {}_H
-        #define {}_H
-        {}
-        #endif // {}_H
-        ]], { i(1), rep(1), i(0), rep(1) }))
-    })
-
-    ls.add_snippets("markdown", {
-        s("code", fmt("```{}\n{}\n```", { i(1), i(0) }))
-    })
-end
-
 return {
     "VonHeikemen/lsp-zero.nvim",
     branch = "v3.x",
@@ -47,16 +5,8 @@ return {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
 
-        -- LSP Support
         "neovim/nvim-lspconfig",
-        -- Autocompletion
-        "hrsh7th/nvim-cmp",
-        "hrsh7th/cmp-nvim-lsp",
         "onsails/lspkind.nvim",
-
-        -- LuaSnip
-        "L3MON4D3/LuaSnip",
-        "L3MON4D3/cmp_luasnip",
 
         "lukas-reineke/lsp-format.nvim",
     },
@@ -69,82 +19,25 @@ return {
         local lsp_format = require("lsp-format")
         local lspkind = require("lspkind")
         local cmp = require("cmp")
-        local luasnip = require("luasnip")
-
-        local function lsp_format_on_attach(client, bufnr)
-            lsp_format.on_attach(client, bufnr)
-        end
 
         lsp_zero.on_attach(function(_, bufnr)
-            -- see :help lsp-zero-keybindings to learn the available actions
             lsp_zero.default_keymaps({ buffer = bufnr })
-            -- lsp_zero.buffer_autoformat()
         end)
-
-        luasnip.setup({
-            history = true,
-            update_events = "TextChanged,TextChangedI",
-            enable_autosnippets = true
-        })
-        setup_custom_snippets(luasnip)
-
-        vim.keymap.set({ "i", "s" }, "<C-j>", function()
-            if luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            end
-        end)
-
-        vim.keymap.set({ "i", "s" }, "<C-k>", function()
-            if luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            end
-        end, { silent = true })
-
-        vim.keymap.set({ "i", "s" }, "<C-l>", function()
-            if luasnip.choice_active() then
-                luasnip.change_choice(1)
-            end
-        end, { silent = true })
 
         vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help)
 
-        cmp.setup({
-            mapping = cmp.mapping.preset.insert({
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-j>"] = cmp.mapping.confirm({ select = true }),
-                ["<C-n>"] = cmp.mapping.select_next_item(),
-                ["<C-p>"] = cmp.mapping.select_prev_item(),
-                ["<C-e>"] = cmp.mapping.close(),
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4)
-            }),
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('my.lsp', {}),
+            callback = function(args)
+                local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+                if client:supports_method('textDocument/completion') then
+                    -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+                    -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+                    -- client.server_capabilities.completionProvider.triggerCharacters = chars
 
-            sources = {
-                { name = "nvim_lua" },
-                { name = "nvim_lsp" },
-                { name = "path" },
-                { name = "luasnip" },
-                { name = "buffer", keyword_length = 5 },
-            },
-
-            formatting = {
-                format = lspkind.cmp_format({
-                    with_text = true,
-                    menu = {
-                        nvim_lsp = "[LSP]",
-                        nvim_lua = "[api]",
-                        luasnip = "[snippet]",
-                        buffer = "[buffer]",
-                        path = "[path]"
-                    }
-                })
-            },
-
-            snippet = {
-                expand = function(args)
-                    vim.snippet.expand(args.body)
-                end,
-            },
+                    vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+                end
+            end
         })
 
         lspkind.init({})
