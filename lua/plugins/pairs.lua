803eb42634346d5935c0068244f89999c4872f7d
spiel0meister <soviczan7@gmail.com>
2025-04-13 17:43:16 +0200

    replace auto-pairs with autoclose

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -1,9 +1,36 @@
 return {
-    "jiangmiao/auto-pairs",
+    "m4xshen/autoclose.nvim",
     dependencies = {
         "kylechui/nvim-surround",
     },
     config = function()
+        require("autoclose").setup({
+            keys = {
+                -- ["{"] = { close = true, escape = true, pair = "{}" },
+                -- ["["] = { close = true, escape = true, pair = "[]" },
+                -- ["("] = { close = true, escape = true, pair = "()" },
+                ["("] = { escape = false, close = true, pair = "()" },
+                ["["] = { escape = false, close = true, pair = "[]" },
+                ["{"] = { escape = false, close = true, pair = "{}" },
+
+                [">"] = { escape = false, close = false, pair = "<>" },
+                [")"] = { escape = false, close = false, pair = "()" },
+                ["]"] = { escape = false, close = false, pair = "[]" },
+                ["}"] = { escape = false, close = false, pair = "{}" },
+
+                ['"'] = { escape = false, close = true, pair = '""' },
+                ["'"] = { escape = false, close = true, pair = "''" },
+                ["`"] = { escape = false, close = true, pair = "``" },
+            },
+            options = {
+                disabled_filetypes = { "text" },
+                disable_when_touch = false,
+                touch_regex = "[%w(%[{]",
+                pair_spaces = false,
+                auto_indent = true,
+                disable_command_mode = false,
+            },
+        })
         require("nvim-surround").setup({})
     end
 }
