69f1b4eb5dc158237f93bb0cbdcb401ebc685889
spiel0meister <soviczan7@gmail.com>
2025-05-02 20:22:28 +0200

    better folding settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -33,9 +33,13 @@ vim.opt.termguicolors = true
 -- transparent fold text (source: https://github.com/neovim/neovim/pull/20750)
 vim.opt.foldtext = ""
 vim.opt.fillchars = "fold: "
+
+-- treesitter for folding
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldenable = false
+vim.opt.foldcolumn = "1"
+vim.opt.foldlevel = 99
+vim.opt.foldenable = true
 
 -- Scroll
 vim.opt.scrolloff = 8
